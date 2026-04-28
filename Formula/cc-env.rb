d6d5c9b64429ca79c5e9b06fb37b85ce6c3d538c
梁康达 <liangkd01@mingyuanyun.com>
2026-04-28 14:05:31 +0800

    chore: update cc-env to 1.2.0
    - Update tarball URL to version 1.2.0
    - Update SHA256 checksum

--- Formula/cc-env.rb
+++ Formula/cc-env.rb
@@ -2,7 +2,7 @@ class CcEnv < Formula
   desc "CLI tool for managing Claude Code runtime environment configurations"
   homepage "https://github.com/lkangd/cc-env"
   url "https://registry.npmjs.org/@lkangd/cc-env/-/cc-env-1.2.0.tgz"
-  sha256 "c8d3eae160a892e32837db3dcae515e843e5383fef52b8141940c8bcf8b6d59f"
+  sha256 "0ca0dcb5eeb4540b48e32714d85dd7804b616534edd134b167e8c23f7c19bd3e"
   license "ISC"
 
   depends_on "node@20"
