9835b965bb26ecd5de60e8cbe4697bba4c01e22f
梁康达 <liangkd01@mingyuanyun.com>
2026-05-23 10:03:02 +0800

    chore: update cc-env to 1.4.1
    - Update tarball URL to version 1.4.1
    - Update SHA256 checksum

--- Formula/cc-env.rb
+++ Formula/cc-env.rb
@@ -2,8 +2,8 @@ class CcEnv < Formula
   desc "CLI tool for managing Claude Code runtime environment configurations"
   homepage "https://github.com/lkangd/cc-env"
   deprecate! date: "2026-05-22", because: :repo_archived, replacement_formula: "cc-settings-preset"
-  url "https://registry.npmjs.org/@lkangd/cc-env/-/cc-env-1.4.0.tgz"
-  sha256 "5ac0a8aaaab259decbaab57e4a4cba9aa105b13784fff5f555861465e0fc5eab"
+  url "https://registry.npmjs.org/@lkangd/cc-env/-/cc-env-1.4.1.tgz"
+  sha256 "c8d3eae160a892e32837db3dcae515e843e5383fef52b8141940c8bcf8b6d59f"
   license "ISC"
 
   depends_on "node@20"
